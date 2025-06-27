cd8ed8351744fc7d71bfe4ab5c8b59b31f056ded
Dez <desmondwebbj@gmail.com>
2025-06-27 16:13:44 -0700

    Fight functions complete

---
+++ src/fightDetailsExtractor.py
@@ -0,0 +1,295 @@
+from bs4 import BeautifulSoup
+import re
+import requests
+import pandas as pd
+
+def getFighters(bout_soup: bs4.BeautifulSoup) -> tuple[str, str]:
+	    
+    '''
+    Fetches a tuple of the bout's fighters' names
+        
+    Parameters:
+    ----------------    
+    
+    BeautifulSoup: unmodified, parsed target bout url
+    
+    Returns:
+    ----------------    
+    
+    tuple (string, string): fighter_1_name, fighter_2_name 
+    '''
+	
+    #confirming that there is data for 2 fighters
+    fighter_n = len(bout_soup.findAll("a", attrs={"class": re.compile("b-link b-fight-details__person-link")}))
+
+    #ensuring the return of null values if fighter(s) absent
+    fighter_1, fighter_2 = None, None
+    
+    if fighter_n == 2:
+		
+        fighter_1 = bout_soup.findAll("a", attrs={"class": re.compile("b-link b-fight-details__person-link")})[0].text.strip()
+		
+        fighter_2 = bout_soup.findAll("a", attrs={"class": re.compile("b-link b-fight-details__person-link")})[1].text.strip()
+        
+    return fighter_1, fighter_2
+
+def getFightWinner(bout_soup: bs4.BeautifulSoup) -> str:
+
+        '''
+    Fetches the winner of bout
+        
+    Parameters:
+    ----------------    
+
+	BeautifulSoup: unmodified, parsed target bout url
+    
+    Returns:
+    ----------------    
+    
+    str: winner of bout
+    '''
+    
+    fighter_headlines = bout_soup.findAll("div", attrs={"class": re.compile('b-fight-details__person')})[0]
+    
+    isolation_list = []
+    
+    for fighter_headline in fighter_headlines:
+        
+        if isinstance(fighter_headline, NavigableString)==False:
+            
+            isolation_list.append(fighter_headline)
+            
+    for headline in isolation_list:
+        
+        winner = headline.find("i", attrs={"class": re.compile("b-fight-details__person-status b-fight-details__person-status_style_green")})
+        
+        if winner:
+            
+            name = headline.find("a", attrs={"class": re.compile("b-link b-fight")}).text.strip()
+            
+    return name
+
+def getFightWeightClass(bout_soup: bs4.BeautifulSoup) -> str:
+    	    
+    '''
+    Fetches a string for the bout's weight class
+        
+    Parameters:
+    ----------------    
+
+	BeautifulSoup: unmodified, parsed target bout url
+    
+    Returns:
+    ----------------    
+    
+    str: weight class of bout
+    '''
+	
+    weight_class_raw = bout_soup.find("i", attrs={"class": re.compile("b-fight-details__fight-title")}).text
+    
+    weight_class_words = weight_class_raw.split()
+
+    weight_class_search = [weight for weight in weight_class_words if weight.endswith('weight')]
+
+    if len(weight_class_search) == 1:
+
+        weight_class = weight_class_search[0]
+        
+    else:
+        
+        weight_class = None
+        
+    # return weight_class
+    return weight_class
+
+def getFightInfo(bout_soup: bs4.BeautifulSoup) -> str:
+        	    
+    '''
+    Fetches a tuple of the bout's fighters' names
+        
+    Parameters:
+    ----------------    
+
+	BeautifulSoup: unmodified, parsed target bout url
+    
+    Returns:
+    ----------------    
+    
+    list: [
+            method, 
+            round bout was stopped,
+            time bout was stopped,
+            fight round format,
+            bout referee,
+            additional notable details
+            ]
+    '''
+	
+    search = bout_soup.findAll("i", attrs={"class": re.compile("b-fight-details__text")})
+
+    values_list = []
+    
+    for detail in search:    
+        
+        if 'Method:' in detail.text:
+            
+            text = detail.text.split(':')[1].strip()
+            
+            if text == '':
+                
+                values_list.append(None)
+                
+            else:
+                
+                values_list.append(text)
+                
+        elif 'Round:' in detail.text:
+            
+            text = detail.text.split(':')[1].strip()
+            
+            if text == '':
+                
+                values_list.append(None)
+                
+            else:
+                
+                values_list.append(text)
+                
+        elif 'Time:' in detail.text:
+            
+            text = detail.text.split(':')[1].strip()
+            
+            if text == '':
+                
+                values_list.append(None)
+                
+            else:
+                
+                values_list.append(text)
+                
+        elif 'Time format:' in detail.text:
+            
+            text = detail.text.split(':')[1].strip()
+            
+            if text == '':
+                
+                values_list.append(None)
+                
+            else:
+                
+                values_list.append(text)
+                
+        elif 'Referee:' in detail.text:
+            
+            text = detail.text.split(':')[1].strip()
+            
+            if text == '':
+                
+                values_list.append(None)
+                
+            else:
+                
+                values_list.append(text)
+                
+        elif 'Details:' in detail.text:
+            
+            text = detail.text.split(':')[1].strip()
+            
+            if text == '':
+                
+                values_list.append(None)
+                
+            else:
+                
+                values_list.append(text)
+                
+    return values_list
+
+def getSigStrkTrgts(bout_soup: bs4.BeautifulSoup) -> str:
+            	    
+    '''
+    Fetches a significant strike percentages for bout
+        
+    Parameters:
+    ----------------    
+
+	BeautifulSoup: unmodified, parsed target bout url
+    
+    Returns:
+    ----------------    
+    
+    tuple (list, list): (
+                            [head1, body1, leg1, distance1, clinch1, ground1], 
+                            [head2, body2, leg2, distance2, clinch2, ground2]
+                        )
+    '''
+    
+    sig_strk_brkdn_list = bout_soup.findAll("i", attrs={"class": re.compile("b-fight-details__charts-num")})
+
+    stats_list = []
+    
+    for stat in sig_strk_brkdn_list:
+        
+        stats_list.append(stat.text.strip())
+        
+    fighter_1_sig_strk_stats = stats_list[:-1:2]
+    
+    fighter_2_sig_strk_stats = stats_list[1::2]
+    
+    return fighter_1_sig_strk_stats, fighter_2_sig_strk_stats
+
+	def extractFightStats(bout_soup: bs4.BeautifulSoup) -> str:
+            	    
+    '''
+    Fetches a summary and round by round stats for a bout
+        
+    Parameters:
+    ----------------    
+
+	BeautifulSoup: unmodified, parsed target bout url
+    
+    Returns:
+    ----------------    
+    
+    tuple (list, list, list, list) : (
+                                        [
+                                            totals,
+                                            totals_pr_rnd,
+                                            sig_strks_totals,
+                                            sig_strks_pr_rnd
+                                        ]
+                                    )
+    '''
+    
+    sections_soups = bout_soup.findAll("tbody", attrs={"class": re.compile("b-fight-details__table")})
+
+    if len(sections_soups) == 4:
+        
+        sections_data = []
+        
+        for section in sections_soups:
+            
+            section_soup = section.findAll("p", attrs={"class": re.compile("b-fight-details__table-text")})
+            
+            section_data = []
+            
+            for item in section_soup:
+                
+                if not item.find('a'):
+                    
+                    section_data.append(item.get_text(strip=True))
+                    
+            sections_data.append(section_data)
+        
+        totals = sections_data[0]
+        
+        totals_pr_rnd = sections_data[1]
+        
+        sig_strks_totals = sections_data[2]
+        
+        sig_strks_pr_rnd = sections_data[3]
+    
+    return totals, totals_pr_rnd, sig_strks_totals, sig_strks_pr_rnd
+
+if __name__ == "__main__":
+    main()
